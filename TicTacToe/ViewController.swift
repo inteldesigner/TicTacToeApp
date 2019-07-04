6b1b8a6dd179bd626365921a2cea63abbadd7f3d
Eric <ericstein.rl@gmail.com>
2019-07-04 10:34:33 -0500

    first commit

---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,98 @@
+//
+//  ViewController.swift
+//  TicTacToeApp
+//
+//  Created by Eric Stein on 7/3/19.
+//  Copyright © 2019 Eric Stein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var winnerLabel: UILabel!
+    @IBOutlet var playAgainButton: UIButton!
+    
+    @IBAction func playAgain(_ sender: AnyObject) {
+        
+        activeGame = true
+        activePlayer = 1
+        gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        
+        for i in 1..<10 {
+            if let button = view.viewWithTag(i) as? UIButton {
+                button.setImage(nil, for: [])
+            }
+            
+            winnerLabel.isHidden = true
+            playAgainButton.isHidden = true
+            
+            winnerLabel.center = CGPoint(x: winnerLabel.center.x - 500, y: winnerLabel.center.y)
+            playAgainButton.center = CGPoint(x: playAgainButton.center.x - 500, y: playAgainButton.center.y)
+        }
+    }
+    
+    // 1 -> cercle, 2 -> crosses
+    var activeGame = true
+    
+    var activePlayer = 1
+    
+    var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0] // 0 = empty, 1 = cercle, 2 = crosses
+    
+    let winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+
+    @IBAction func buttonPressed(_ sender: AnyObject) {
+        
+        let activePosition = sender.tag - 1
+        
+        if gameState[activePosition] == 0 && activeGame {
+            gameState[activePosition] = activePlayer
+        
+            if activePlayer == 1 {
+                sender.setImage(UIImage(named: "nought.png"), for: [])
+                activePlayer = 2
+            } else {
+                sender.setImage(UIImage(named: "cross.png"), for: [])
+                activePlayer = 1
+            }
+            
+            for combination in winningCombinations {
+                if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]] {
+                    
+                    // If winner
+                    activeGame = false
+                    winnerLabel.isHidden = false
+                    playAgainButton.isHidden = false
+                    
+                    if gameState[combination[0]] == 1 {
+                        winnerLabel.text = "Noughts won!"
+                    } else {
+                        winnerLabel.text = "Crosses won!"
+                    }
+                    
+                    UIView.animate(withDuration: 1, animations: {
+                        self.winnerLabel.center = CGPoint(x: self.winnerLabel.center.x + 500, y: self.winnerLabel.center.y)
+                        self.playAgainButton.center = CGPoint(x: self.playAgainButton.center.x + 500, y: self.playAgainButton.center.y)
+                    })
+                }
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        winnerLabel.isHidden = true
+        playAgainButton.isHidden = true
+        
+        winnerLabel.center = CGPoint(x: winnerLabel.center.x - 500, y: winnerLabel.center.y)
+        playAgainButton.center = CGPoint(x: playAgainButton.center.x - 500, y: playAgainButton.center.y)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
